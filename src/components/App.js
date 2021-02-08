61984b21bd34c66df67737fe0e2e1bcd0de82837
Dorian Brown <devorian143@gmail.com>
2021-02-08 07:58:49 -0800

    CSS Reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react"; 
 import { Switch, Route } from "react-router-dom";
+import Nav from "./Nav/Nav";
 import HomePageContainer from "./Home/HomePageContainer";
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={HomePageContainer} />
+      <Route exact path="/home" component={HomePageContainer} />
+      {/* <Route exact path="/calculator" component={CalculatorContainer} /> */}
+      <Route path="*" component={HomePageContainer} />
     </Switch>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => { 
+    return (
+        <nav className="navbar">
+            <div className="navbar__home"></div>
+        </nav>
+    )
+};
+
+export default Nav;
