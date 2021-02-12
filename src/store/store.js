8b8073bd071522dab1158d98859261a60cf92aa5
Dorian Brown <devorian143@gmail.com>
2021-02-11 17:05:49 -0800

    Fixes

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux"; 
-import logger from "redux-logger"; 
+// import logger from "redux-logger"; 
 import rootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => { 
-    return createStore(rootReducer, preloadedState, applyMiddleware(logger));
+    // return createStore(rootReducer, preloadedState, applyMiddleware(logger));
+    return createStore(rootReducer, preloadedState);
 };
 
 export default configureStore;
