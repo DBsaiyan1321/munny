a47981a1525e869a574dd81156fb06f563bf8c48
Dorian Brown <devorian143@gmail.com>
2021-02-15 16:51:44 -0800

    Risk Level Redux

--- src/actions/risk_actions.js
+++ src/actions/risk_actions.js
@@ -1,3 +1,8 @@
+import { riskLevels } from "../util/riskLevels";
+
+export const RECEIVE_RISK_LEVEL = "RECEIVE_RISK_LEVEL";
+
 export const setRiskLevel = level => ({
-    type: level
+    type: RECEIVE_RISK_LEVEL, 
+    payload: riskLevels[level]
 });
--- src/reducers/risk_reducer.js
+++ src/reducers/risk_reducer.js
@@ -1,4 +1,4 @@
-import { riskLevels } from "../util/riskLevels";
+import { RECEIVE_RISK_LEVEL } from "../actions/risk_actions";
 
 // const defaultState = {
 //     selectLevel: 20
@@ -7,26 +7,8 @@ import { riskLevels } from "../util/riskLevels";
 const riskReducer = (initialState = {}, action) => { 
     Object.freeze(initialState); 
     switch (action.type) {
-        case 1:
-            return { ...initialState, ...riskLevels[1]};
-        case 2:
-            return { ...initialState, ...riskLevels[2] };
-        case 3:
-            return { ...initialState, ...riskLevels[3] };
-        case 4:
-            return { ...initialState, ...riskLevels[4] };
-        case 5:
-            return { ...initialState, ...riskLevels[5] };
-        case 6:
-            return { ...initialState, ...riskLevels[6] };
-        case 7:
-            return { ...initialState, ...riskLevels[7] };
-        case 8:
-            return { ...initialState, ...riskLevels[8] };
-        case 9:
-            return { ...initialState, ...riskLevels[9] };
-        case 10:
-            return { ...initialState, ...riskLevels[10] };
+        case RECEIVE_RISK_LEVEL: 
+            return action.payload;
         default:
             return initialState;
     }
